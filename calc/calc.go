9a1f501356c9d0f5d967676994fcb2c0d0850c3b
orionvega2343-cloud <orionvega2343@gmail.com>
2026-06-26 18:51:10 +0300

    feat: Add calculator cli

--- calc/calc.go
+++ calc/calc.go
@@ -1,1 +1,67 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func usage() {
+	fmt.Fprintf(os.Stderr, "usage: calc <number> [<number> <number> ...]\n")
+	flag.PrintDefaults()
+	os.Exit(2)
+}
+
+var (
+	op = flag.String("op", "add", "operator [add/sub/mul/div]")
+)
+
+func main() {
+	log.SetFlags(0)
+	log.SetPrefix("calc: ")
+
+	flag.Usage = usage
+	flag.Parse()
+
+	args := flag.Args()
+
+	if len(args) != 2 {
+		usage()
+	}
+
+	a := args[0]
+	b := args[1]
+
+	parseA, err := strconv.Atoi(a)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	parseB, err := strconv.Atoi(b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch *op {
+	case "add":
+		fmt.Printf("%d + %d = %d\n", parseA, parseB, parseA+parseB)
+
+	case "sub":
+		fmt.Printf("%d - %d = %d\n", parseA, parseB, parseA-parseB)
+
+	case "mul":
+		fmt.Printf("%d  * %d = %d\n", parseA, parseB, parseA*parseB)
+
+	case "div":
+		if parseB == 0 {
+			log.Fatal("division by zero")
+		}
+		fmt.Printf("%d/%d = %d\n", parseA, parseB, parseA/parseB)
+
+	default:
+		usage()
+	}
+
+}
